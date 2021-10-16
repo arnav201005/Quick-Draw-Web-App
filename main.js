d6dac68cf7181749ae7cc0c5e8cde0717c65a9f8
arnav201005 <78555984+arnav201005@users.noreply.github.com>
2021-10-16 20:03:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function setup()
+{
+ canvas = createCanvas(310,310);
+ canvas.center();
+ background("white");
+}
+
+function clearcanvas()
+{
+background("white");
+
+}
+function preload()
+{
+
+}
+
+function draw()
+{
+
+}
+
+function classifyCanvas()
+{
+    
+}
